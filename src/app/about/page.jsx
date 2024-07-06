0cab62e898a0ca9447ac0594b3617a57bf4aed27
MirzaSameer301 <sameerijaz301@gamil.com>
2024-07-06 18:07:30 +0500

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-3">
+      <h1  className="text-center text-amber-600 font-medium text-2xl">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non nulla
+        incidunt, odit fuga nostrum omnis perspiciatis iste blanditiis, quia
+        enim ipsum doloribus explicabo dignissimos maxime error ea aspernatur
+        totam numquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non nulla
+        incidunt, odit fuga nostrum omnis perspiciatis iste blanditiis, quia
+        enim ipsum doloribus explicabo dignissimos maxime error ea aspernatur
+        totam numquam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non nulla
+        incidunt, odit fuga nostrum omnis perspiciatis iste blanditiis, quia
+        enim ipsum doloribus explicabo dignissimos maxime error ea aspernatur
+        totam numquam.
+      </p>
+
+     
+    </div>
+  );
+}
