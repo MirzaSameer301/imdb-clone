22e0f678cbd0d7741feebb6e63c5a803973da3cb
MirzaSameer301 <sameerijaz301@gamil.com>
2024-07-06 17:24:19 +0500

    add dark mode

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function MenuItem({title,address,Icon}) {
   return (
-    <Link href={address} className='text-slate-700 hover:text-amber-500'>
+    <Link href={address} className=' hover:text-amber-500'>
     <Icon className='text-2xl sm:hidden'/>
     <p className=' uppercase hidden sm:inline text-xl'>{title}</p>
     </Link>
