267a3b49d0c11c4610c88795272047126e167ecc
MirzaSameer301 <sameerijaz301@gamil.com>
2024-07-07 19:02:12 +0500

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import { FiThumbsUp } from "react-icons/fi";
+
+export default function Card({ result }) {
+  return (
+    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+      <Link href={`/movie/${result.id}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          alt="pic"
+          height={300}
+          width={500}
+          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
+        />
+        <div className="p-2">
+          <p className=" line-clamp-2">{result.overview}</p>
+          <h2 className="text-lg font-bold truncate">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center gap-2">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="ml-2" />
+            {result.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
