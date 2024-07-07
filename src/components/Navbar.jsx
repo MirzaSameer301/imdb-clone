b5193968c61088f84da5dcb45942208fa9d8dd44
MirzaSameer301 <sameerijaz301@gamil.com>
2024-07-07 17:14:39 +0500

    fetching data from tmdb api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className='bg-amber-100 dark:bg-gray-600 justify-center p-4 flex gap-6 lg:text-lg' >
         <NavbarItems title='Trending' param='fetchTrending'/>
-        <NavbarItems title='Top Rated' param='fetchTopRates'/>
+        <NavbarItems title='Top Rated' param='fetchTopRated'/>
     </div>
   )
 }
